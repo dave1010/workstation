f04f882fc41a273d2250eedc705b4df5df1b8e83
Dave Hulbert <dave@wearebase.com>
2013-03-26 10:09:36 +0000

    Install Compass (Sass framework)

--- install/sass.sh
+++ install/sass.sh
@@ -1,11 +1,19 @@
 
-# phpstorm
+# sass
 if ! which sass > /dev/null; then
     echo "Installing Sass"
     gem install sass
 else
     echo "Sass already installed"
 fi
+
+if ! which compass > /dev/null; then
+    echo "Installing Compass"
+	gem install compass
+else
+    echo "Compass already installed"
+fi
+
 
 #Note: If there is an error you probably need to add the following line 
 #export PATH=$PATH:/var/lib/gems/1.8/bin  at the end of your ./bashrc
