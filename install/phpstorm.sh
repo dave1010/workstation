a7b6d607232ba21e02726cd199d7f2ad5d3a627f
Dave Hulbert <dave@wearebase.com>
2013-03-26 10:10:15 +0000

    Update PhpStorm to 6.0

--- install/phpstorm.sh
+++ install/phpstorm.sh
@@ -3,8 +3,9 @@
 # phpstorm
 if ! which /usr/local/phpstorm/bin/phpstorm.sh > /dev/null; then
     echo "Installing PhpStorm"
-    PHPSTORM=$(curl http://confluence.jetbrains.net/pages/viewpage.action?pageId=41487696|grep -o 'http[^"]*PhpStorm[^"]*tar.gz'|head -n 1)
-    wget $PHPSTORM
+    #PHPSTORM=$(curl http://confluence.jetbrains.net/pages/viewpage.action?pageId=41487696|grep -o 'http[^"]*PhpStorm[^"]*tar.gz'|head -n 1)
+    #wget $PHPSTORM
+    wget http://download.jetbrains.com/webide/PhpStorm-6.0.tar.gz
     tar xvf PhpStorm*
     rm PhpStorm-*.tar.gz
     mv PhpStorm* /usr/local/phpstorm
