2fe72d8119e555b3033b8f53de210d1c6ba8c96a
Dave Hulbert <dave@wearebase.com>
2013-04-03 12:25:20 +0100

    Update sublime text install to v3

--- install/sublime-text.sh
+++ install/sublime-text.sh
@@ -1,9 +1,16 @@
 # sublime text
-if ! which sublime-text-2 > /dev/null; then
+
+
+if ! which /usr/bin/subl > /dev/null; then
     echo "Installing Sublime Text"
-    add-apt-repository -y ppa:webupd8team/sublime-text-2
-    apt-get update
-    apt-get install -y sublime-text
+	# install from http://www.sublimetext.com/3
+	wget $(curl -s http://www.sublimetext.com/3 | grep 'Ubuntu 64 bit' | grep -Po 'ht[^"]+' | grep deb)
+	dpkg -i sublime-text*.deb
+
+	# old PPA only has ST2
+    #add-apt-repository -y ppa:webupd8team/sublime-text-2
+    #apt-get update
+    #apt-get install -y sublime-text
 else
     echo "Sublime Text already installed"
 fi
