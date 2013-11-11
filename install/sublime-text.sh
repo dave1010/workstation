06be4469215a3c5590256191e16c364551861359
Jonathan Ginn <jon@jonginn.com>
2013-11-11 10:57:29 +0000

    Update sublime-text.sh
    Add sublime text 3 repo

--- install/sublime-text.sh
+++ install/sublime-text.sh
@@ -3,9 +3,13 @@
 
 if ! which /usr/bin/subl > /dev/null; then
     echo "Installing Sublime Text"
+    	add-apt-repository -y ppa:webupd8team/sublime-text-3
+    	apt-get update
+    	apt-get install -y sublime-text-installer
+    
 	# install from http://www.sublimetext.com/3
-	wget $(curl -s http://www.sublimetext.com/3 | grep 'Ubuntu 64 bit' | grep -Po 'ht[^"]+' | grep deb)
-	dpkg -i sublime-text*.deb
+	#wget $(curl -s http://www.sublimetext.com/3 | grep 'Ubuntu 64 bit' | grep -Po 'ht[^"]+' | grep deb)
+	#dpkg -i sublime-text*.deb
 
 	# old PPA only has ST2
     #add-apt-repository -y ppa:webupd8team/sublime-text-2
